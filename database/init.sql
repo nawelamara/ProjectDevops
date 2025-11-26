54d5561836bdfc177a8c1f6e80064c1400b86f32
nawelamara <100841834+nawelamara@users.noreply.github.com>
2025-11-26 19:03:03 +0100

    Revise users table structure and insert new records

--- database/init.sql
+++ database/init.sql
@@ -1,6 +1,10 @@
--- Créer la table users
-CREATE TABLE IF NOT EXISTS users ( id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, email TEXT NOT NULL UNIQUE );
--- Insérer des utilisateurs 
-INSERT INTO users (name, email) VALUES ('Nawel ', 'nawel.amara@example.com'),
-  ('Monia', 'monia@example.com'),
-  ('Melek ', 'melek@example.com');
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL
+);
+
+INSERT INTO users (name, email) VALUES
+    ('Mariem', 'Mariem@example.com'),
+  ('Malek', 'Malek@example.com'),
+    ('Nawel', 'Nawel@example.com');
