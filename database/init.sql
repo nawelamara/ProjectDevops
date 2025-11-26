990ce5668551dd924004231a95e724d07b6c0256
nawelamara <100841834+nawelamara@users.noreply.github.com>
2025-11-26 18:26:51 +0100

    Add users table and sample data to init.sql
    Create the users table and insert sample users.

--- database/init.sql
+++ database/init.sql
@@ -1,1 +1,6 @@
-
+-- Créer la table users
+CREATE TABLE IF NOT EXISTS users ( id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, email TEXT NOT NULL UNIQUE );
+-- Insérer des utilisateurs 
+INSERT INTO users (name, email) VALUES ('Nawel ', 'nawel.amara@example.com'),
+  ('Monia', 'monia@example.com'),
+  ('Melek ', 'melek@example.com');
